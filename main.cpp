5e0ef573787a7abd9a2811ee3a7f35ed3c281b40
MylenaGabrielle <mylenagabrielle1@gmail.com>
2026-06-04 05:52:34 +0000

    program finished

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 
 using namespace std;
 int main() {
-    
+
+    //Create my memory space
+    int* age = new int;
+
+    //program ask your age
+    cout << "What is your age? ";
+
+    //User input your age, incluid address with *
+    cin >> *age;
+    cout << "I am  " << *age << " years old" << endl;
+
+    //deallocation
+    delete age;
+    age = nullptr;
+
+    return 0;
 }
